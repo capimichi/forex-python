3d7282587ba8d12d4098420d3d0a073e316e17da
Michele Capicchioni <capimichi@gmail.com>
2021-07-10 12:45:59 +0200

    Modifica

--- src/cm_forex_python/forex.py
+++ src/cm_forex_python/forex.py
@@ -1,2 +1,12 @@
 def calculate_top_shadow(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
     return df.apply(lambda x : (x[high_col] - x[open_col]) if (x[open_col] > x[close_col]) else (x[high_col] - x[close_col]), axis = 1)
+
+def calculate_bottom_shadow(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
+    return df.apply(lambda x : (x[close_col] - x[low_col]) if (x[open_col] > x[close_col]) else (x[open_col] - x[low_col]), axis = 1)
+
+def calculate_body(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
+    return df.apply(lambda x : (x[open_col] - x[close_col]) if (x[open_col] > x[close_col]) else (x[close_col] - x[open_col]), axis = 1)
+
+def calculate_direction(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
+    return df.apply(lambda x : -1 if (x[open_col] > x[close_col]) else (1 if(x[open_col] < x[close_col]) else 0), axis = 1)
+
