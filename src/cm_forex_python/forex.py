7c16995415c3274e8a1e6d015e6689e959193aa6
Michele Capicchioni <capimichi@gmail.com>
2021-07-10 16:41:41 +0200

    Modifica

--- src/cm_forex_python/forex.py
+++ src/cm_forex_python/forex.py
@@ -10,12 +10,12 @@ def calculate_body(df, open_col = 'open', close_col = 'close', low_col = 'low', 
 def calculate_direction(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
     return df.apply(lambda x : -1 if (x[open_col] > x[close_col]) else (1 if(x[open_col] < x[close_col]) else 0), axis = 1)
 
-def calculate_max_increment(df, step):
-  max_increment = abs(df.shift(step).open - df.rolling(step).high.max())
-  max_increment = max_increment.shift(-10)
+def calculate_max_increment(df, step, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
+  max_increment = abs(df.shift(step)[open_col] - df.rolling(step)[high_col].max())
+  max_increment = max_increment.shift(step * -1)
   return max_increment
 
-def calculate_max_decrement(df, step):
-  max_increment = abs(df.shift(step).open - df.rolling(step).low.min())
-  max_increment = max_increment.shift(-10)
+def calculate_max_decrement(df, step, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
+  max_increment = abs(df.shift(step)[open_col] - df.rolling(step)[low_col].min())
+  max_increment = max_increment.shift(step * -1)
   return max_increment
