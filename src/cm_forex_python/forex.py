726229eb585c71d909e06120d3bd06161176c1ca
Michele Capicchioni <capimichi@gmail.com>
2021-07-10 17:31:54 +0200

    Modifica

--- src/cm_forex_python/forex.py
+++ src/cm_forex_python/forex.py
@@ -19,3 +19,11 @@ def calculate_max_decrement(df, step, open_col = 'open', close_col = 'close', lo
   max_increment = abs(df.shift(step)[open_col] - df.rolling(step)[low_col].min())
   max_increment = max_increment.shift(step * -1)
   return max_increment
+
+def check_for_order(df, step, take_profit_pips, stop_loss_pips, pip_size = 0.0001):
+  increment = calculate_max_increment(df, step)
+  decrement = calculate_max_decrement(df, step)
+  new_df = pd.DataFrame({'increment': increment, 'decrement': decrement})
+  tp = take_profit_pips * pip_size
+  sl = stop_loss_pips * pip_size
+  return new_df.apply(lambda x : 1 if(x.increment >= tp and x.decrement < sl) else (-1 if (x.decrement >= tp and x.increment < sl) else 0), axis = 1)
