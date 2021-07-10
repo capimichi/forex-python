af7c6650e604fbe8a53f57fb78556f24f5bf20c1
Michele Capicchioni <capimichi@gmail.com>
2021-07-10 12:25:04 +0200

    4

--- src/cm_forex_python/forex.py
+++ src/cm_forex_python/forex.py
@@ -1,3 +1,2 @@
-
 def calculate_top_shadow(df, open_col = 'open', close_col = 'close', low_col = 'low', high_col = 'high'):
     return df.apply(lambda x : (df[high_col] - df[open_col]) if (df[open_col] > df[close_col]) else (df[high_col] - df[close_col]))
