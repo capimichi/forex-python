8663bc1ad2e5f009e18cdc511b13f71c2a58eb29
Michele Capicchioni <capimichi@gmail.com>
2021-07-12 21:41:12 +0200

    Modifica

--- src/cm_forex_python/forex.py
+++ src/cm_forex_python/forex.py
@@ -29,3 +29,32 @@ def check_for_order(df, step, take_profit_pips, stop_loss_pips, pip_size = 0.000
   tp = take_profit_pips * pip_size
   sl = stop_loss_pips * pip_size
   return new_df.apply(lambda x : 1 if(x.increment >= tp and x.decrement < sl) else (-1 if (x.decrement >= tp and x.increment < sl) else 0), axis = 1)
+
+
+def count_support_resistance(df, step, support_resistance=0, pip_size=0.0001, pip_threshold=5, compare_columns=None,
+                             open_col='open', close_col='close', low_col='low', high_col='high'):
+    if (compare_columns == None):
+        compare_columns = [open_col, close_col, high_col, low_col]
+
+    pip_threshold_value = pip_size * pip_threshold
+
+    counts = []
+
+    for i in range(0, len(df.index)):
+        rows = df.iloc[max(0, i - step):i]
+        count = 0
+        close = df.iloc[i].close
+
+        for index, past_row in rows.iterrows():
+            increment = False
+            for compare_column in compare_columns:
+                column_value = past_row[compare_column]
+                check_value = abs(close - column_value)
+
+                if (check_value < pip_threshold_value):
+                    increment = True
+            if (increment):
+                count += 1
+        counts.append(count)
+
+    return counts
